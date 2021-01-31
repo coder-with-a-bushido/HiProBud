5928ca98c739be768971a7704e440b2162abe063
coder-with-a-bushido <karthikeyanssvk@gmail.com>
2021-01-31 16:48:09 +0530

    token made private.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ import net.dv8tion.jda.api.JDABuilder;
 
 public class Main {
     public static void main(String[] args) {
-        JDABuilder builder = JDABuilder.createDefault("214e02b781def2526b38f773b2c567b8c489f4c4e6a16c8bcf8506868e2a12bf");
+        JDABuilder builder = JDABuilder.createDefault(Token.token);
     }
 }
